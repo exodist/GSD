a1e168c9c11f123067995e70d44abb8e18d8983b
Chad Granum <exodist7@gmail.com>
2013-02-12 22:37:47 -0800

    Start a spec for the stress test

--- Structures/src/include/gsd_dict.h
+++ Structures/src/include/gsd_dict.h
@@ -11,7 +11,7 @@ typedef struct dict_methods  dict_methods;
 typedef struct dict dict;
 
 typedef void   (dict_change)( dict *d, void *meta, void *key, void *old_val, void *new_val );
-typedef void   (dict_ref)( dict *d, void *meta, void *ref, int offset );
+typedef void   (dict_ref)( dict *d, void *meta, void *ref, int delta );
 typedef int    (dict_handler)( void *key, void *value, void *args );
 typedef int    (dict_cmp)( dict_settings *s, void *key1, void *key2 );
 typedef size_t (dict_loc)( dict_settings *s, void *key );
