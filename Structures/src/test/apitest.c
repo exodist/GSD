05d990c68b6b508fe4b5c2047ec591c9ba380aeb
Chad Granum <exodist7@gmail.com>
2013-03-03 15:11:10 -0800

    Initial api test stub

---
+++ Structures/src/test/apitest.c
@@ -0,0 +1,108 @@
+#include "../include/gsd_dict.h"
+#include "../include/gsd_dict_return.h"
+#include "structure.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
+#include <time.h>
+
+typedef struct kv kv;
+struct kv {
+    uint64_t  value;
+    size_t    refcount;
+    uint64_t  fnv_hash;
+};
+
+kv NKV = { 0, 0, 0 };
+
+// If true FNV will be used to locate and compare keys, otherwise the raw
+// integer value is used (which is a worst case scenario as that means sorted
+// insert into all the trees.
+int USE_FNV = 1;
+
+kv    *new_kv( uint64_t val );
+void   kv_ref( dict *d, void *ref, int delta );
+size_t kv_loc( size_t slot_count, void *meta, void *key );
+int    kv_cmp( void *meta, void *key1, void *key2 );
+void   kv_change( dict *d, void *meta, void *key, void *old_val, void *new_val );
+uint64_t hash_bytes( uint8_t *data, size_t length );
+
+kv *new_kv( uint64_t val ) {
+    kv *it = malloc( sizeof( kv ));
+    memset( it, 0, sizeof( kv ));
+    it->refcount = 1;
+    it->value = val;
+    return it;
+}
+
+void kv_change( dict *d, void *meta, void *key, void *old_val, void *new_val ) {
+    return;
+}
+
+void kv_ref( dict *d, void *ref, int delta ) {
+    if ( ref == NULL ) return;
+    if ( ref == &NKV ) return;
+    kv *r = ref;
+    assert( r->refcount != 0 );
+    size_t refs = __sync_add_and_fetch( &(r->refcount), delta );
+    if ( refs == 0 ) free( ref );
+}
+
+size_t kv_loc( size_t slot_count, void *meta, void *key ) {
+    kv *k = key;
+
+    if ( !USE_FNV ) return k->value % slot_count;
+
+    if ( !k->fnv_hash ) {
+        k->fnv_hash = hash_bytes( (void *)&(k->value), sizeof( k->value ));
+    }
+    return k->fnv_hash % slot_count;
+}
+
+int kv_cmp( void *meta, void *key1, void *key2 ) {
+    if ( key1 == key2 ) return 0;
+
+    kv *k1 = key1;
+    kv *k2 = key2;
+
+    if ( USE_FNV ) {
+        if ( !k1->fnv_hash ) {
+            k1->fnv_hash = hash_bytes( (void *)&(k1->value), sizeof( k1->value ));
+        }
+        if ( !k2->fnv_hash ) {
+            k2->fnv_hash = hash_bytes( (void *)&(k2->value), sizeof( k2->value ));
+        }
+
+        if ( k1->fnv_hash > k2->fnv_hash ) return -1;
+        if ( k1->fnv_hash < k2->fnv_hash ) return  1;
+    }
+
+    if ( k1->value > k2->value ) return -1;
+    if ( k1->value < k2->value ) return  1;
+
+    return 0;
+}
+
+uint64_t hash_bytes( uint8_t *data, size_t length ) {
+    uint64_t seed = 14695981039346656037UL;
+
+    if ( length < 1 ) return seed;
+
+    uint64_t key = seed;
+    uint64_t i;
+    for ( i = 0; i < length; i++ ) {
+        key ^= data[i];
+        key *= 1099511628211;
+    }
+
+    return key;
+}
+
+int main() {
+    
+    return 1;
+}
+
