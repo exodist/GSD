547c98266c6c49a161f5a67f1cca5fd720c40d54
Chad Granum <exodist7@gmail.com>
2013-04-03 21:32:31 -0700

    More features for webdocs, more data

--- spec.js
+++ spec.js
@@ -15,11 +15,13 @@ $( function() {
                     item.addClass( 'active' )
 
                     build_content( data );
+                    fixView();
                 },
                 error: function() {
                     $('#view').html( '<div class="error">Error loading page</div>' )
                     $( '.nav' ).removeClass( 'active' )
                     item.addClass( 'active' )
+                    fixView();
                 }
             }
         )
@@ -34,6 +36,13 @@ $( function() {
         $( '#about' ).trigger( 'click' )
     }
 })
+
+function fixView() {
+    var height = $('#subnav').outerHeight();
+    if ( height < 350 ) height = 350;
+    $('#view').css( 'min-height', height );
+    console.log( $(this).outerHeight(), height );
+}
 
 function build_content( data ) {
     var view = $( '#view' );
@@ -59,15 +68,30 @@ function build_content( data ) {
 
             process( id, viewitem );
 
-            navitem.click( function() {
+            var normal_click = function() {
                 view.children().hide();
                 $('ul.second_subnav').hide();
                 subnav.children().removeClass( 'active' );
-                navitem.addClass( 'active' );
                 viewitem.show();
-                $('ul#SN-' + id).show();
-                $('ul#SN-' + id).children().removeClass('active');
-            });
+                var sn = $('ul#SN-' + id);
+                if ( sn.length ) {
+                    sn.show();
+                    sn.children().removeClass('active');
+                    subnav.children().hide();
+                    navitem.unbind( 'click' );
+                    navitem.click( function() {
+                        subnav.children().show();
+                        navitem.unbind( 'click' );
+                        navitem.click( normal_click );
+                        fixView();
+                    });
+                }
+                navitem.addClass( 'active' );
+                navitem.show();
+                fixView();
+            };
+
+            navitem.click( normal_click );
 
             subnav.append( navitem );
             view.append( viewitem );
@@ -97,9 +121,11 @@ function process( id, container ) {
                 dataType: 'json',
                 success: function( data ) {
                     list.replaceWith( build_symbol_list( data ));
+                    fixView();
                 },
                 error: function(blah, message1, message2) {
                     $('#view').append( '<div class="error">Error loading ' + list.attr( 'src' ) + '</div>' )
+                    fixView();
                 }
             }
         )
@@ -115,9 +141,11 @@ function process( id, container ) {
                 success: function( data ) {
                     list.detach();
                     build_sub_list( id, data );
+                    fixView();
                 },
                 error: function(blah, message1, message2) {
                     $('#view').append( '<div class="error">Error loading ' + list.attr( 'src' ) + '</div>' )
+                    fixView();
                 }
             }
         )
@@ -140,10 +168,13 @@ function build_sub_list( pid, data ) {
 
 function build_sub_list_item( pid, navkey, data, nav, subnav ) {
     var navname    = data[navkey]["name"];
+    if ( !navname ) navname = navkey;
     var desc       = data[navkey]["desc"];
     var roles      = data[navkey]["roles"];
     var attributes = data[navkey]["attributes"];
     var methods    = data[navkey]["methods"];
+    var requires   = data[navkey]["requires"];
+    var usage       = data[navkey]["usage"];
 
     var navitem = $(
         '<li id="' + navkey + '"><a href="' + nav[0] + '-' + pid + '-' + navkey + '">' + navname + '</a></li>'
@@ -152,13 +183,23 @@ function build_sub_list_item( pid, navkey, data, nav, subnav ) {
         '<div style="display: none"><h2>' + navname + '</h2>' + desc + '</div>'
     );
 
+    if ( usage ) {
+        viewitem.append( '<h3>Usage</h3>' );
+        viewitem.append( usage );
+    }
+
     if ( roles ) {
         viewitem.append( '<h3>Roles:</h3>' );
         var list = $('<ul class="roll_list"></ul>');
         for (role in roles) {
-            list.append( '<li>' + roles[role] + '</li>' );
+            list.append( '<li><a href="' + nav[0] + '-roles-' + roles[role] + '" onclick="openRole(\'' + roles[role] + '\')">' + roles[role] + '<a></li>' );
         }
         viewitem.append( list );
+    }
+
+    if ( requires ) {
+        viewitem.append( '<h3>Required Methods:</h3>' );
+        viewitem.append( build_symbol_list( requires ));
     }
 
     if ( attributes ) {
@@ -176,6 +217,7 @@ function build_sub_list_item( pid, navkey, data, nav, subnav ) {
         subnav.children().removeClass( 'active' );
         navitem.addClass( 'active' );
         viewitem.show();
+        fixView();
     });
 
     subnav.append( navitem );
@@ -185,10 +227,26 @@ function build_sub_list_item( pid, navkey, data, nav, subnav ) {
 function build_symbol_list( data ) {
     var table = $( '<table class="symbol_list"><tbody><tr><th>Name</th><th>Description</th></tr></tbody></table>' );
     for (key in data) {
+        var name = data[key]['name'];
+        if ( !name ) name = key;
         var row = $( '<tr class="symbol" onclick="expandDesc(this)"></tr>' );
-        row.append( $('<td class="left">' + data[key]['name'] + '</td>') );
+        row.append( $('<td class="left">' + name + '</td>') );
         row.append( $('<td class="right">' + data[key]['desc'] + '</td>') );
-        var drow = $( '<tr class="symbol_details" style="display: none;"><td colspan="2">' + data[key]['details'] + '</td>' );
+
+        var details = $( '<td colspan="2"></td>' );
+        if ( data[key]['usage'] ) {
+            var list = $( '<ul style="usage"></ul>' );
+            for ( i in data[key]['usage'] ) {
+                var item = $( '<li><span style="example">' + data[key]['usage'][i] + '</span></li>' );
+                list.append( item );
+            }
+            details.append( list );
+        }
+        details.append( data[key]['details'] );
+
+        var drow = $( '<tr class="symbol_details" style="display: none;"></tr>' );
+        drow.append( details );
+
         table.append( row );
         table.append( drow );
     }
@@ -199,4 +257,10 @@ function build_symbol_list( data ) {
 function expandDesc( e ) {
     $(e).toggleClass( 'open' );
     $(e).next().toggle();
+}
+
+function openRole( role ) {
+    $('#main_subnav').find('#roles').trigger( 'click' );
+    $('#SN-roles').find('#' + role).trigger( 'click' );
+    fixView();
 }
